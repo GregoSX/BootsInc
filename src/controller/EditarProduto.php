d0765d95386adf062a1d9f9a4913efb7c90e6faf
Guilherme Grego <guilhermegrego376@gmail.com>
2022-09-06 03:44:43 -0300

    Adicionando styles

--- src/controller/EditarProduto.php
+++ src/controller/EditarProduto.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/styleEditar.css">
+    <title>Editar Produto</title>
+</head>
+<body>
+<div class="container">
 <form name="form1" method="POST" action="Incluir.php">
 <?php
 
@@ -14,7 +25,7 @@ include_once '../persistence/ProdutoDAO.PHP';
           WHERE codProduto=".$_GET['codProduto'];
     $result = mysqli_query($conn, $sql);
     $vetor = mysqli_fetch_array($result, MYSQLI_ASSOC);
-?>
+?>    
   <input type="hidden" name="codProduto" value="<?php echo $_GET['codProduto'];  ?>">
   <table border="0" align="center" width="35%">
         <tr><td width="20%">Código:</td>
@@ -24,8 +35,9 @@ include_once '../persistence/ProdutoDAO.PHP';
         </tr>
         <tr><td width="20%">Descrição:</td>
             <td colspan="2" width="90%">
-            <input type="text" name="descricao" value="<?php echo @$vetor['descricao']; ?>" maxlength="30" size="30">
-        </td>
+                <input type="text" name="descricao" value="<?php echo @$vetor['descricao']; ?>" maxlength="30" size="30">
+            </td>
+        </tr>
         <tr><td width="20%">Preço:</td>
             <td colspan="2" width="90%">
             <input type="number" step="0.01" min="0.01" max="9999.99" name="preco" value="<?php echo @$vetor['preco']; ?>" size="8">
@@ -40,9 +52,13 @@ include_once '../persistence/ProdutoDAO.PHP';
             <input type="number" name="quantidadeEstoque" value="<?php echo @$vetor['quantidadeEstoque']; ?>" size="8">
         </td>
         </tr>
-        <tr><td colspan="3" align="center">
+        <tr><td colspan="3" align="center" class="buttons">
             <input type="submit" value="Salvar">
             <input type="button" value="Cancelar" onclick="location.href='./ListarProduto.php'">
             </td>
         </tr>
 </table>
+</form>
+</div>
+</body>
+</html>
