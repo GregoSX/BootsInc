5fa610782ba346c0c1ad6a740ea743f8feb8d541
Guilherme Grego <guilhermegrego376@gmail.com>
2022-09-19 11:23:02 -0300

    Adicionado casos de testes e correção em ExcluirProdutos

--- src/controller/controllerProduto/ExcluirProduto.php
+++ src/controller/controllerProduto/ExcluirProduto.php
@@ -1,7 +1,7 @@
 <?php
 
-include_once '../persistence/BD.php';
-include_once '../persistence/ProdutoDAO.PHP';
+include_once '../../persistence/BD.php';
+include_once '../../persistence/ProdutoDAO.PHP';
 
 $codProduto = $_GET['codProduto'];
 
